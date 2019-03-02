901b5452436e19793a168c50c846ddfbe75e6ce7
Jeremy38100 <jeremyroche5@gmail.com>
2019-03-02 16:54:24 +0100

    giraffe

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -1,107 +1,122 @@
 let cryBaby = new Audio('assets/sounds/cry.ogg');
 let happyBaby = new Audio('assets/sounds/happy.ogg');
-let smile = "assets/images/faces/smile.png";
-let sad = "assets/images/faces/sad.png";
-let cry = "assets/images/faces/cry.png";
-let tongue = "assets/images/faces/tongue.png";
-let nipple = "assets/images/faces/nipple.png";
-let surprised = "assets/images/faces/surprised.png";
-let sun = "assets/images/thoughts/sun.png";
-let hamburger = "assets/images/thoughts/hamburger.png";
-let intero = "assets/images/thoughts/intero.png";
-let poo = "assets/images/thoughts/poo.png"
-let moods = [cry, sad, smile];
-let needs = [hamburger, poo];
+let smile = "assets/images/faces/happygiraffe.png";
+let hungry = "assets/images/faces/hungrygiraffe.png";
+let bored = "assets/images/faces/boredgiraffe.png";
+let oups = "assets/images/faces/oupsgiraffe.png";
+let empty = "assets/images/thoughts/empty.png";
+let hamburger = "assets/images/thoughts/002-branch.png";
+let intero = "assets/images/thoughts/001-beach-ball.png";
+let poo = "assets/images/thoughts/003-poo.png"
+let moods = [bored, smile, oups, hungry];
 let gameOver = document.getElementById("gameover");
 let gameOn = document.getElementById("gameon");
-let idBaby = document.getElementById("baby");
-let idNeeds = document.getElementById("needs");
+let baby = document.getElementById("baby");
+let needs = document.getElementById("needs");
+let resultat = document.getElementById("resultat")
 let stop;
 let score = 0;
 let progress = document.getElementById("progress");
 
 pickMood = function () {
-    idBaby.src = smile;
-    idNeeds.src = sun;
-    // happyBaby.play();
+    baby.src = smile;
+    baby.classList.remove("animated", "infinite", "shake");
+    needs.src = empty;
+    happyBaby.play();
     setTimeout(() => {
-        if (idBaby.src.endsWith(smile)) {
-            idBaby.src = moods[Math.floor(Math.random() * 3)];
-            if ((idBaby.src.endsWith(cry))) {
-                idNeeds.src = needs[Math.floor(Math.random() * 2)];
-                cryBaby.play();
-                perdu();
-                progress.classList.add("progress-bar-danger");
-            }
-            else if ((idBaby.src.endsWith(sad))) {
-                idNeeds.src = intero;
-                perdu();
-                progress.classList.add("progress-bar-danger");
-
-            }
-            else {
+        if (baby.src.endsWith(smile)) {
+            baby.src = moods[Math.floor(Math.random() * 4)];
+            if ((baby.src.endsWith(bored))) {
+                needs.src = intero;
+                needAppear();
+            } else if ((baby.src.endsWith(hungry))) {
+                needs.src = hamburger;
+                needAppear();
+            } else if ((baby.src.endsWith(oups))) {
+                needs.src = poo;
+                needAppear();
+            } else {
                 pickMood();
                 stopPerdu();
             }
         }
-    }, 1000);
+    }, 3000);
 };
 
+function needAppear() {
+    needs.classList.remove("slideOutRight");
+    needs.classList.add("slideInRight");
+    baby.classList.add("animated", "infinite", "shake");
+    happyBaby.pause();
+    cryBaby.play();
+    perdu();
+    progress.classList.add("progress-bar-danger");
+}
+
 function perdu() {
-    // stop = setTimeout(() => {
-    //     gameOver.style.display = "block";
-    //     gameOn.style.display = "none";
-    // }, 6000);
+    stop = setTimeout(() => {
+        gameOver.style.display = "block";
+        document.getElementById("gameOverText").style = '-webkit-animation : unzoom 0.5s';
+        gameOn.style.display = "none";
+    }, 6000);
 };
 
 function stopPerdu() {
     clearTimeout(stop);
 };
 
-function Feed() {
-    if (idNeeds.src.endsWith(hamburger)) {
-        score++;
-        updateScore();
-        idBaby.src = nipple;
-        idNeeds.src = sun;
-        relanceJeu()
-        progress.classList.remove("progress-bar-danger");
+function feed() {
+    if (needs.src.endsWith(hamburger)) {
+        goodAnswer();
     } else {
-        score--;
-        updateScore();
+        wrongAnswer();
     }
 };
 
-function Play() {
-    if (idNeeds.src.endsWith(intero)) {
-        score++;
-        updateScore();
-        idBaby.src = tongue;
-        idNeeds.src = sun;
-        relanceJeu()
-        progress.classList.remove("progress-bar-danger");
+function play() {
+    if (needs.src.endsWith(intero)) {
+        goodAnswer();
     } else {
-        score--;
-        updateScore();
+        wrongAnswer();
     }
 };
 
-function Change() {
-    if (idNeeds.src.endsWith(poo)) {
-        score++;
-        updateScore();
-        idBaby.src = surprised;
-        idNeeds.src = sun;
-        relanceJeu()
-        progress.classList.remove("progress-bar-danger");
+function change() {
+    if (needs.src.endsWith(poo)) {
+        goodAnswer();
     } else {
-        score--;
-        updateScore();
+        wrongAnswer();
     }
-};
+}
+
+function goodAnswer() {
+    score++;
+    updateScore();
+    baby.src = smile;
+    cryBaby.pause();
+    needs.classList.remove("slideInRight");
+    needs.classList.add("slideOutRight");
+    progress.classList.remove("progress-bar-danger");
+    stopPerdu();
+    pickMood();
+}
+
+function wrongAnswer() {
+    animScore();
+    score--;
+    updateScore();
+}
+
+function animScore() {
+    resultat.classList.add("heartBeat");
+    removeAnim = setTimeout(() => {
+        resultat.classList.remove("heartBeat");
+    }, 500);
+}
 
 function updateScore() {
-    document.getElementById("resultat").innerHTML = score;
+    resultat.innerHTML = score;
+    animScore();
 }
 
 function resetScore() {
@@ -109,19 +124,12 @@ function resetScore() {
     updateScore();
 }
 
-function relanceJeu() {
-    setTimeout(() => {
-        stopPerdu();
-        pickMood();
-    }, 1000);
-}
-
-function Reset() {
-    idBaby.src = smile;
-    idNeeds.src = sun;
+function reset() {
+    baby.src = smile;
+    needs.src = empty;
     gameOver.style.display = "none";
     gameOn.style.display = "block";
-    resetScore()
+    resetScore();
     pickMood();
     progress.classList.remove("progress-bar-danger");
 };
